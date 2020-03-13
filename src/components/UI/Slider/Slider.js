7fe32f5cb7c0290ed0721f951d0acbc3f1ade057
Igor Kornilov <gaber32188@gmail.com>
2020-03-13 13:57:14 +0300

    fixed Slider

--- src/components/UI/Slider/Slider.js
+++ src/components/UI/Slider/Slider.js
@@ -5,8 +5,9 @@ import { diff } from 'semver';
 
 export default function Slider(props) {
   const { images } = props;
+  const imageWidth = 300;
   const sliderContainer = useRef();
-  const [currentImg, setCurrentImg] = useState(0);
+  const [currentImg, setCurrentImg] = useState(1);
   const [btnOpcatity, setBtnOpcaticy] = useState(0);
   const [isDown, setIsDown] = useState(false);
   const mouseStatsRef = useRef({ offset: 0, initialX: null });
@@ -14,9 +15,11 @@ export default function Slider(props) {
   const transitionCss = {
     transition: `transform 400ms cubic-bezier(0.175, 0.885, 0.32, 1.275`
   };
+  useEffect(() => {
+    mouseStatsRef.current.offset = -currentImg * imageWidth;
+  }, [currentImg]);
 
   const onClickHandler = e => {
-    console.log('there');
     const curAtrb = e.target.getAttribute('desc');
     if (curAtrb === 'prev') {
       if (currentImg === 0) {
@@ -28,6 +31,8 @@ export default function Slider(props) {
         setCurrentImg(0);
       } else setCurrentImg(currentImg + 1);
     }
+
+    console.log(sliderContainer.current.getBoundingClientRect().x - imageWidth);
   };
 
   const enterHandler = () => {
@@ -43,13 +48,17 @@ export default function Slider(props) {
     if (!isDown) {
       return;
     }
-
     let currentOffset = e.clientX - mouseStatsRef.current.initialX;
 
     mouseStatsRef.current.initialX = e.clientX;
     mouseStatsRef.current.offset += currentOffset;
 
     sliderContainer.current.style.transform = `translateX(${mouseStatsRef.current.offset}px)`;
+
+    console.log(
+      { ...mouseStatsRef.current },
+      sliderContainer.current.getBoundingClientRect()
+    );
   };
 
   const mouseDownHandler = e => {
@@ -74,7 +83,8 @@ export default function Slider(props) {
         ref={sliderContainer}
         className={styles.Images}
         style={{
-          transform: `translateX(-${currentImg * (100 / images.length)}%)`,
+          // transform: `translateX(-${currentImg * (100 / images.length)}%)`,
+          transform: `translateX(-${(1800 / images.length) * currentImg}px)`,
           transition: `${!isDown ? transitionCss.transition : null}`
         }}>
         {images &&
@@ -109,3 +119,26 @@ export default function Slider(props) {
     </div>
   );
 }
+// const dragHandler = e => {
+//   if (!isDown) {
+//     return;
+//   }
+
+//   let currentOffset = e.clientX - mouseStatsRef.current.initialX;
+
+//   mouseStatsRef.current.initialX = e.clientX;
+//   mouseStatsRef.current.offset += currentOffset;
+
+//   let regex = /(\d+|-\d+)/gi;
+//   let currentTranslatePx = Number(
+//     sliderContainer.current.style.transform.match(regex)
+//   );
+//   console.log(currentTranslatePx);
+
+//   sliderContainer.current.style.transform = `translateX(${mouseStatsRef.current.offset}px)`;
+
+//   console.log(
+//     { ...mouseStatsRef.current },
+//     sliderContainer.current.getBoundingClientRect()
+//   );
+// };
