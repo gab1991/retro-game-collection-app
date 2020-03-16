949af83b96ab4794f131515fd8fb89b0b729dcea
Igor Kornilov <gaber32188@gmail.com>
2020-03-16 23:55:09 +0300

    Card flip basic

--- src/components/AuthModal/AuthModal.js
+++ src/components/AuthModal/AuthModal.js
@@ -10,25 +10,48 @@ export default function AuthModal(props) {
   return (
     <div className={styles.AuthModal}>
       <Backdrop />
-      <div className={styles.Modal}>
-        <h1>Seen you lately?</h1>
-        <form onSubmit={submitHandler}>
-          <div>
-            <label>Username</label>
-            <input type="text" name="username" id="username" />
-          </div>
-          <div>
-            <label>Password</label>
-            <input type="text" name="password" id="password" />
-          </div>
-          <div>
-            <ButtonNeon txtContent={`Continue as Guest`} rectangular blinking />
-          </div>
-          <div className={styles.BtnSection}>
-            <ButtonNeon txtContent={`Sign in`} rectangular />
-            <ButtonNeon txtContent={`Sign Up`} rectangular />
-          </div>
-        </form>
+      <div className={styles.AuthCard}>
+        <div className={styles.SignIn}>
+          <h1>Seen you lately?</h1>
+          <form onSubmit={submitHandler}>
+            <div>
+              <label>Username</label>
+              <input type="text" name="username" id="username" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="text" name="password" id="password" />
+            </div>
+            <div>
+              <ButtonNeon
+                txtContent={`Continue as Guest`}
+                rectangular
+                blinking
+              />
+            </div>
+            <div className={styles.BtnSection}>
+              <ButtonNeon txtContent={`Sign in`} rectangular />
+              <ButtonNeon txtContent={`Sign Up`} rectangular />
+            </div>
+          </form>
+        </div>
+        <div className={styles.SignUp}>
+          <h1>Seen you lately?</h1>
+          <form onSubmit={submitHandler}>
+            <div>
+              <label>Username</label>
+              <input type="text" name="username" id="username" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="text" name="password" id="password" />
+            </div>
+            <div className={styles.BtnSection}>
+              <ButtonNeon txtContent={`Sign in`} rectangular />
+              <ButtonNeon txtContent={`Sign Up`} rectangular />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
