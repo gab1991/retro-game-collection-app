15a87050f72f92faf85a178e6bbd0e3aeae36570
Igor Kornilov <gaber32188@gmail.com>
2020-03-28 20:05:13 +0300

    nav css rebuild

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,20 @@
-import React from "react";
-import styles from "./Layout.css";
+import React from 'react';
+import styles from './Layout.css';
 
 export default function Layout(props) {
   return (
     <div className={styles.Layout}>
       {props.children}
       <div className={styles.Footer}>
-        <footer>All right reserved</footer>
+        <footer>
+          <a href="https://rawg.io/" target="_blank">
+            RAWG
+          </a>{' '}
+          is the source of the data and screenshots / boxarts are provided by{' '}
+          <a href="https://emumovies.com/" target="_blank">
+            emumovies.com
+          </a>
+        </footer>
       </div>
     </div>
   );
