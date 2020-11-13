694852d26b5b2025f44278df06e8889187ecb771
Igor Kornilov <gaber32188@gmail.com>
2020-11-13 14:46:15 +0300

    “R page changes #DEMO-20” Done

--- src/components/AuthModal/AuthModal.js
+++ src/components/AuthModal/AuthModal.js
@@ -22,6 +22,8 @@ export default function AuthModal(props) {
 
   const hideAuthModal = () => {};
 
+  console.log('test');
+
   return (
     <div className={styles.AuthModal}>
       <div className={`${styles.AuthCard} ${styles[activeSide]}`}>
