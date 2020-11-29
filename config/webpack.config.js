eabb3bda71bc627d40085f4f62faac537d8a2c46
Igor.Kornilov <gaber32188@gmail.com>
2020-11-29 18:14:12 +0300

    remove ts and tsx from babel loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -367,7 +367,7 @@ module.exports = function (webpackEnv) {
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
-              test: /\.(js|mjs|jsx|ts|tsx)$/,
+              test: /\.(js|mjs|jsx)$/,
               include: paths.appSrc,
               loader: require.resolve('babel-loader'),
               options: {
