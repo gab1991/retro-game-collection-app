095ca5fc42591337def7ac309cd798d9fd9182c7
Igor.Kornilov <gaber32188@gmail.com>
2020-12-05 15:17:23 +0300

    reexports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { GameSelector, PlatformSelector } from 'Routes';
 
 import GameDetailed from './Components/GameDetailed/GameDetailed';
 import Layout from './Components/Layout/Layout';
 import Profile from './Components/Profile/Profile';
 import useWindowSize from './CustomHooks/useWindowSize';
-import GameSelector from './Routes/GameSelector/GameSelector';
 import { setIsMobile } from './Store/Actions/appStateActions';
 import { checkCredentials } from './Store/Actions/authActions';
 import { getProfileInfo } from './Store/Actions/profileActions';
-import { PlatformSelector } from 'Routes/PlatformSelector/PlatformSelector';
 
 import styles from './App.module.scss';
 import sassVars from './Сonfigs/Variables.scss';
