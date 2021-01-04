99c60aaa208f7cbfdab503653f0b24fdf981ff3e
Igor.Kornilov <gaber32188@gmail.com>
2021-01-04 13:03:18 +0300

    Preview EbayCard types

--- src/typings/EbayData.ts
+++ src/typings/EbayData.ts
@@ -7,3 +7,34 @@ export type TEbayCardItemData = {
   itemUrl: string;
   pictures: Array<string>;
 };
+
+export type TEbayCardPreviewRawData = {
+  autoPay: Array<string>;
+  condition: Array<Array<string>>;
+  country: Array<string>;
+  galleryURL: Array<string>;
+  globalId: Array<string>;
+  isMultiVariationListing: Array<string>;
+  itemId: Array<string>;
+  listingInfo: Array<Array<string>>;
+  location: Array<string>;
+  postalCode: Array<string>;
+  primaryCategory: Array<Array<string>>;
+  productId: Array<{ '@type': string; __value__: string }>;
+  returnsAccepted: Array<string>;
+  sellingStatus: Array<{
+    convertedCurrentPrice: Array<IEbayDataCurrencyValue>;
+    currentPrice: Array<IEbayDataCurrencyValue>;
+    sellingState: Array<string>;
+    timeLeft: Array<string>;
+  }>;
+  shippingInfo: Array<{ shippingServiceCost: Array<IEbayDataCurrencyValue> }>;
+  title: Array<string>;
+  topRatedListing: Array<string>;
+  viewItemURL: Array<string>;
+};
+
+interface IEbayDataCurrencyValue {
+  '@currencyId': string;
+  __value__: string;
+}
