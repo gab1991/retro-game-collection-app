73d172963c6ce032e70240cba66e2f24dc9dbe89
Igor.Kornilov <gaber32188@gmail.com>
2021-01-24 15:00:06 +0300

    set up basic dev conf

--- webpack/nodeInterface.js
+++ webpack/nodeInterface.js
@@ -1,35 +1,45 @@
-// import webpack from 'webpack';
 const webpack = require('webpack');
 const config = require('./webpack.config');
 
+//devserver
+
+const WebpackDevServer = require('webpack-dev-server');
+const devServerOptions = { ...config.devServer };
+
+// my mode
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const isProduction = mode === 'development' ? false : true;
+const isDevelopment = !isProduction;
+
 const compiler = webpack({ ...config });
 
-compiler.run((err, stats) => {
-  if (err) {
-    console.log('CONFIG ERROR', { ...err });
-  }
-  if (stats.hasErrors()) {
-    console.log('COMPILE ERROR', { ...stats });
-  }
-  console.log(
-    stats.toString({
-      preset: 'normal',
-      colors: true,
-      assetsSort: '!size',
-      chunksSort: '!size',
-    })
-  );
-});
+console.log('NODE INTERFACE MODE =', mode);
 
-// const watching = compiler.watch(
-//   {
-//     // Example [watchOptions](/configuration/watch/#watchoptions)
-//     aggregateTimeout: 300,
-//     poll: undefined,
-//   },
-//   (err, stats) => {
-//     // [Stats Object](#stats-object)
-//     // Print watch/build result here...
-//     console.log(stats);
-//   }
-// );
+if (isProduction) {
+  compiler.run((err, stats) => {
+    if (err) {
+      console.log('CONFIG ERROR', { ...err });
+    }
+    if (stats.hasErrors()) {
+      console.log('COMPILE ERROR', { ...stats });
+    }
+    console.log(
+      stats.toString({
+        preset: 'normal',
+        colors: true,
+        assetsSort: '!size',
+        chunksSort: '!size',
+      })
+    );
+  });
+}
+
+if (isDevelopment) {
+  const server = new WebpackDevServer(compiler, devServerOptions);
+
+  server.listen(devServerOptions.port, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
