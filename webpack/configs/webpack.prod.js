954afdaafeb6ffeab516b8e5f154f93345f882d4
Igor.Kornilov <gaber32188@gmail.com>
2021-02-13 12:36:59 +0300

    fix terser conf err

--- webpack/configs/webpack.prod.js
+++ webpack/configs/webpack.prod.js
@@ -2,18 +2,13 @@ const { merge } = require('webpack-merge');
 const { createCommon } = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const zlib = require('zlib');
 
 const prodConfig = merge(
   {
     optimization: {
       usedExports: true,
       minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          cache: true,
-        }),
-      ],
+      minimizer: [new TerserPlugin()],
     },
     plugins: [new CompressionPlugin({})],
   },
