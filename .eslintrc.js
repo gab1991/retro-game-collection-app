9888f0d5f59f905650c5d500bac74c341a0ef88b
Igor.Kornilov <gaber32188@gmail.com>
2021-02-22 15:01:38 +0300

    modify complexity rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,8 +29,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'sort-keys-fix', 'typescript-sort-keys'],
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
@@ -112,7 +112,8 @@ module.exports = {
       },
     ],
     // SONAR
-    'sonarjs/cognitive-complexity': 'warn',
+
+    'sonarjs/cognitive-complexity': ['warn', 20],
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
