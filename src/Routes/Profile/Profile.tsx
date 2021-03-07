f81d545f60827bfbcec3d5145f63aabb4603daa1
Igor.Kornilov <gaber32188@gmail.com>
2021-03-07 10:40:50 +0300

    Profile to ts

--- src/Routes/Profile/Profile.tsx
+++ src/Routes/Profile/Profile.tsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router';
 
 import CollectionList from './CollictionList/CollectionLIst';
 import WishList from './WishList/WishList';
+import { selectLoggedUser } from 'Store/authReducer/selectors';
 import { getProfileInfo } from 'Store/profileReducer/thunks';
 
 import styles from './Profile.module.scss';
 
-function _Profile(props) {
-  const { loggedUser } = props;
-  const { section } = props.match.params;
-  const [activeSection, setActiveSection] = useState(section || 'CollectionList');
+enum EProfileSections {
+  CollectionList = 'CollectionList',
+  WishList = 'WishList',
+}
+
+const AVAILABLE_SECTION = {
+  [EProfileSections.CollectionList]: <CollectionList />,
+  [EProfileSections.WishList]: <WishList />,
+};
+
+export function Profile(): JSX.Element {
+  const loggedUser = useSelector(selectLoggedUser);
+  const { section } = useParams<{ section: EProfileSections }>();
+  const history = useHistory();
   const dispatch = useDispatch();
+  const [activeSection, setActiveSection] = useState(section || EProfileSections.CollectionList);
 
   useEffect(() => {
     if (loggedUser) {
@@ -19,45 +32,38 @@ function _Profile(props) {
     }
   }, [loggedUser, activeSection, dispatch]);
 
-  const sectionToggler = (e) => {
-    const name = e.target.getAttribute('data-desc');
+  const sectionToggler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    const name = e.currentTarget.getAttribute('data-desc');
+
+    if (!isProfileSectionName(name)) {
+      return;
+    }
+
     setActiveSection(name);
-    props.history.push(`/profile/${name}`);
+    history.push(`/profile/${name}`);
   };
 
   return (
     <section className={styles.Profile}>
       <nav className={styles.NavTabs}>
         <ul>
-          <li
-            data-desc={'CollectionList'}
-            onClick={sectionToggler}
-            className={activeSection === 'CollectionList' ? styles.active : ''}
-          >
-            My Collection
+          <li className={activeSection === EProfileSections.CollectionList ? styles.active : ''}>
+            <button onClick={sectionToggler} data-desc={EProfileSections.CollectionList}>
+              My Collection
+            </button>
           </li>
-          <li
-            data-desc={'WishList'}
-            onClick={sectionToggler}
-            className={activeSection === 'WishList' ? styles.active : ''}
-          >
-            Wish List
+          <li className={activeSection === EProfileSections.WishList ? styles.active : ''}>
+            <button onClick={sectionToggler} data-desc={EProfileSections.WishList}>
+              Wish List
+            </button>
           </li>
         </ul>
       </nav>
-      <div className={styles.Content}>
-        {activeSection === 'CollectionList' && <CollectionList />}
-        {activeSection === 'WishList' && <WishList />}
-      </div>
+      <div className={styles.Content}>{AVAILABLE_SECTION[activeSection]}</div>
     </section>
   );
 }
 
-function mapStateToProps(state) {
-  return {
-    loggedUser: state.logged.username,
-    profileInfo: state.profile,
-  };
-}
-
-export const Profile = connect(mapStateToProps)(_Profile);
+const isProfileSectionName = (name: unknown): name is EProfileSections => {
+  return typeof name === 'string' ? Object.keys(EProfileSections).includes(name) : false;
+};
