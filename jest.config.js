7457c6420999ce071c5844bdb9c8bc79ccfca5ca
Igor.Kornilov <gaber32188@gmail.com>
2021-03-14 17:36:05 +0300

    basic jest setup

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  verbose: true, //Indicates whether each individual test should be reported during the run
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', //enables ts parsing
+  },
+};
---
+++ src/components/UI/Togglers/KnobToggler/KnobToggler.test.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { KnobToggler } from '.';
+
+describe('<LoginForm />', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
+    // ???
+  });
+});
