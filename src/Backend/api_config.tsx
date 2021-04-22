2f25dc6a427d00d7920b54c1c503fb1cdbf8f4cf
Igor.Kornilov <gaber32188@gmail.com>
2021-04-22 21:22:43 +0300

    fix fields

--- src/Backend/api_config.tsx
+++ src/Backend/api_config.tsx
@@ -1,9 +1,11 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const apiEndPoint = isProduction ? '/api' : 'http://localhost:8000/api';
+// const apiEndPoint = isProduction ? '/api' : 'http://127.0.0.1:8000/api';
+const apiEndPoint = isProduction ? '/api' : 'http://192.168.31.51:8000/api';
 
 export const api = {
   appServer: {
     boxArtworkUrl: `${apiEndPoint}/box_arts`,
+    checkCredentialUrl: `${apiEndPoint}/auth/check_credentials`,
     ebayItemsUrl: `${apiEndPoint}/ebay/searchList`,
     ebaySingleItemUrl: `${apiEndPoint}/ebay/singleItem`,
     gamesForPlatformUrl: `${apiEndPoint}/rawg/gamesForPlatform`,
