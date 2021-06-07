9a9dbd5cbb36277390597cfe7227e9d002cee17a
Igor.Kornilov <gaber32188@gmail.com>
2021-06-07 23:26:21 +0300

    add basic context to ebayCard

--- src/Components/EbayItemCard/EbayItemCard.tsx
+++ src/Components/EbayItemCard/EbayItemCard.tsx
@@ -9,35 +9,20 @@ import { selectEbayCard } from 'Store/ebayItemsReducer/selectors';
 import { checkIfCardIsWatched, getEbaySingleItemByIndex } from 'Store/ebayItemsReducer/thunks';
 
 import { EbayCardLeftPart, EbayCardRightPart } from './components';
+import { EbayCardContextProvier, IEbayCardContextProvierProps } from './context';
 import { EbayItemCardSkeleton } from './EbayItemCard.skeleton';
 
 import styles from './EbayItemCard.module.scss';
 
-interface IEbayItemCardProps {
-  game: string;
-  index: number;
-  platform: TPlatformNames;
-  sortOrder: EEbaySortOrder;
-}
-
-export function EbayItemCard(props: IEbayItemCardProps): JSX.Element {
-  const dispatch = useDispatch();
-  const { index, platform, game, sortOrder = EEbaySortOrder.Relevance } = props;
-  const card = useSelector((state: IRootState) => selectEbayCard(state, { game, index, platform, sortOrder }));
-  const itemData = card?.itemData;
-
-  useEffect(() => {
-    batch(() => {
-      dispatch(getEbaySingleItemByIndex(platform, game, index, sortOrder));
-      dispatch(checkIfCardIsWatched(game, platform, index, sortOrder));
-    });
-  }, []);
-
+type TEbayItemCardProps = IEbayCardContextProvierProps;
+export function EbayItemCard(props: TEbayItemCardProps): JSX.Element {
   return (
-    <div className={styles.EbayItemCard}>
-      {itemData && <EbayCardLeftPart itemData={itemData} />}
-      {card && itemData && <EbayCardRightPart {...props} card={card} itemData={itemData} index={index} />}
-      {!itemData && <EbayItemCardSkeleton />}
-    </div>
+    <EbayCardContextProvier {...props}>
+      <div className={styles.EbayItemCard}>
+        {/* {itemData && <EbayCardLeftPart itemData={itemData} />}
+      {card && itemData && <EbayCardRightPart {...props} card={card} itemData={itemData} index={index} />} */}
+        {/* {!itemData && <EbayItemCardSkeleton />} */}
+      </div>
+    </EbayCardContextProvier>
   );
 }
---
+++ src/Components/EbayItemCard/context.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode, useContext, useEffect } from 'react';
+import { batch, useDispatch, useSelector } from 'react-redux';
+import { EEbaySortOrder } from 'Api';
+
+import { IRootState } from 'Store/types';
+import { DeepReadonly } from 'utility-types';
+
+import { TPlatformNames } from 'Configs/appConfig';
+import { selectEbayCard } from 'Store/ebayItemsReducer/selectors';
+import { checkIfCardIsWatched, getEbaySingleItemByIndex } from 'Store/ebayItemsReducer/thunks';
+import { TEbayCard } from 'Typings/EbayData';
+
+interface IEbayCardContext {
+  card: DeepReadonly<TEbayCard> | null;
+  itemData: DeepReadonly<TEbayCard['itemData']> | null;
+}
+
+export const EbayCardContext = React.createContext<null | IEbayCardContext>(null);
+
+export interface IEbayCardContextProvierProps {
+  children?: ReactNode;
+  game: string;
+  index: number;
+  platform: TPlatformNames;
+  sortOrder: EEbaySortOrder;
+}
+
+export const EbayCardContextProvier = (props: IEbayCardContextProvierProps): JSX.Element => {
+  const { game, index, platform, sortOrder, children } = props;
+  const dispatch = useDispatch();
+  const card = useSelector((state: IRootState) => selectEbayCard(state, { game, index, platform, sortOrder }));
+  const itemData = card ? card.itemData : null;
+
+  useEffect(() => {
+    batch(() => {
+      dispatch(getEbaySingleItemByIndex(platform, game, index, sortOrder));
+      dispatch(checkIfCardIsWatched(game, platform, index, sortOrder));
+    });
+  }, []);
+
+  return <EbayCardContext.Provider value={{ card, itemData }}>{children}</EbayCardContext.Provider>;
+};
+
+export const useEbayCardContext = () => {
+  const context = useContext(EbayCardContext);
+
+  if (!context) {
+    throw new Error('useEbayCardContext has to be used within EbayCardContext');
+  }
+
+  return context;
+};
